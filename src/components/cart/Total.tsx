e78c3fbd0138241b84ee39ff9a718e2639116790
Tonipenyallop <kimtesu521@gmail.com>
2022-04-21 16:22:19 +0900

    update:price from localstorage

--- src/components/cart/Total.tsx
+++ src/components/cart/Total.tsx
@@ -6,16 +6,29 @@ import {
 } from "../../app/gettingFunctions";
 export default function Total({ allItems, tempCart }: any) {
   function getTotal(): number {
-    return allItems.reduce((a: any, b: any) => {
-      let price = parseInt(getChosenItemPrice(b).split("€")[1]);
-      const quantity = getQuantity(b, tempCart);
-      price *= quantity;
+    return keys.reduce((a: any, b: any) => {
+      // let price = parseInt(getChosenItemPrice(b).split("€")[1]);
+      // const quantity = getQuantity(b, tempCart);
+      // price *= quantity;
+      // return a + price;
+      let price = parseInt(items[b].price.split("€")[1]);
+
+      // let price = parseInt(getChosenItemPrice(b).split("€")[1]);
+      // const quantity = getQuantity(b, tempCart);
+      // price *= quantity;
       return a + price;
     }, 0);
   }
+  let items: any = window.localStorage.getItem("items");
+  items = JSON.parse(items);
+  const keys: any = [];
+  for (let key in items) {
+    console.log(key);
+    keys.push(key);
+  }
   return (
     <div className="">
-      {allItems.length === 0 ? (
+      {keys.length === 0 ? (
         <div className=""></div>
       ) : (
         <div className="flex flex-col justify-start items-center text-white bg-gray-700 w-56 h-56 pt-4 mx-5">
