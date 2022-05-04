a518091fbf07b55447ff3d753dbf8dd07fff0594
Tonipenyallop <kimtesu521@gmail.com>
2022-05-04 12:35:45 +0900

    Update: transition color

--- src/components/userpage/Login.tsx
+++ src/components/userpage/Login.tsx
@@ -3,31 +3,32 @@ import React from "react";
 export default function Login() {
   return (
     <div className="relative top-[145px]">
-      <p className="text-center">YOUR ACCOUNT</p>
-      <div className="flex justify-around items-center border-8">
-        <div className="border-4 flex flex-col justify-center items-center w-[50%]">
-          <div className="">NEW CLIENT</div>
-          <div className=" break-words">
-            Create an account to track & manage orders, view your account &
+      <p className="text-center text-4xl my-3">YOUR ACCOUNT</p>
+      <div className="flex justify-around items-center">
+        <div className="border-r-2 flex flex-col justify-center items-center w-[50%]">
+          <div className="text-2xl my-10">NEW CLIENT</div>
+          <div className="mx-5 break-words">
+            Create an account to track & manage orders, view your account &{" "}
+            <br />
             loyalty information, as well as manage your Wishlist.
           </div>
-          <button className="button">CREATE ACCOUNT</button>
+          <button className="button my-5">CREATE ACCOUNT</button>
         </div>
 
-        <div className="border-4 flex flex-col justify-center items-center w-[50%]">
-          <div className="">ALREADY HAVE AN ACCOUNT?</div>
+        <div className="border-l-2- flex flex-col justify-center items-center w-[50%]">
+          <div className="text-2xl my-10">ALREADY HAVE AN ACCOUNT?</div>
           <input
-            className="border-b-2 focus:border-b-2 focus:border-black focus:outline-none"
+            className="border-b-2 focus:border-b-2 focus:border-black outline-none my-3 transition-color duration-500"
             type="text"
             placeholder="*Email Address"
           />
           <input
-            className="border-b-2 focus:border-b-2 focus:border-black focus:outline-none"
+            className="border-b-2 focus:border-b-2 focus:border-black outline-none my-3 transition-color duration-500"
             type="password"
             placeholder="*Password"
           />
 
-          <button className="button">CREATE ACCOUNT</button>
+          <button className="button my-5">SIGN IN</button>
         </div>
       </div>
     </div>
