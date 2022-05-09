ce80e763df0247fe31cf073ae880c58f2d8efed9
Tonipenyallop <kimtesu521@gmail.com>
2022-05-09 15:19:09 +0900

    Feat: display items user liked

--- src/components/userpage/Userpage.tsx
+++ src/components/userpage/Userpage.tsx
@@ -1,11 +1,40 @@
 import React, { useState } from "react";
 
 export default function Userpage({ isLogin, setIsLogin }: any) {
+  const likedLocal: any = window.localStorage.getItem("liked");
+  const parsedLocalLiked = JSON.parse(likedLocal);
+
+  function getItemLists(): any {
+    const arr = [];
+    for (let key in parsedLocalLiked) {
+      if (parsedLocalLiked[key].liked) {
+        const item = (
+          <div className="border-4 flex justify-start items-center">
+            <img
+              className="w-[150px]"
+              src={parsedLocalLiked[key].url}
+              alt="item I like"
+            />
+            <p>{parsedLocalLiked[key].name}</p>
+            <button className="button">Remove</button>
+          </div>
+        );
+
+        arr.push(item);
+      }
+    }
+
+    return arr;
+  }
+
   return (
     <div className="relative top-[150px]">
       WELCOME MATE
       <div className="">ORDER CONFIRMATION</div>
-      <div className="">FRAGRANCES I LIKE</div>
+      <div className="">
+        <p>FAVORITAS</p>
+        {getItemLists()}
+      </div>
       <button
         className="button"
         onClick={() => {
