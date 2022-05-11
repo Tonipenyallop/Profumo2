da31cb6c02cecb9c81668d5905dbce5911fcbebb
Tonipenyallop <kimtesu521@gmail.com>
2022-05-11 15:16:04 +0900

    Refactor

--- src/components/orders/Orders.tsx
+++ src/components/orders/Orders.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+
 import axios from "axios";
-import OrderTemplate from "./OrderTemplate";
+
 import UserpageMenubar from "../userpage/UserpageMenubar";
+import OrderElement from "./OrderElement";
 export default function Orders() {
-  // const [orderNumber, setOrderNumber] = useState<number>(0);
-  const navigate = useNavigate();
   const [address, setAddress] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [order, setOrder] = useState<any>([]);
@@ -17,33 +16,6 @@ export default function Orders() {
     setEmail(emailLocal);
     getOrder();
   }, []);
-
-  function getItem(items: any): any {
-    const arr = [];
-    for (let key in items) {
-      const item = (
-        <div className="flex justify-start items-start" key={key}>
-          <div className="flex w-[50%]">
-            <img
-              className="w-[150px]"
-              src={items[key]?.url}
-              alt="item I like"
-            />
-            <div className="flex flex-col justify-start items-start">
-              <p className="text-xl font-bold">{items[key]?.name}</p>
-              <p className="text-gray-400">Quantity: {items[key]?.quantity}</p>
-            </div>
-          </div>
-
-          <div className="flex justify-end items-end w-[50%] ">
-            <p className="flex mx-5 font-bold text-md">{items[key]?.price}</p>
-          </div>
-        </div>
-      );
-      arr.push(item);
-    }
-    return arr;
-  }
 
   async function getOrder(): Promise<void> {
     const request = await axios.post("/get_order", { email: emailLocal });
@@ -58,55 +30,14 @@ export default function Orders() {
     setAddress(response.data);
   }
 
-  function getTotalCost(items: any): number {
-    let result = 0;
-    for (let key in items) {
-      const price = parseInt(items[key].price.split("€")[1]);
-      result += price;
-    }
-    return result;
-  }
-
   return (
     <div className="relative top-[150px] ">
       <UserpageMenubar />
       <p className="text-2xl mx-10 py-3">ORDERS</p>
 
       {order.map((e: any, idx: number) => {
-        return (
-          <div
-            className="flex justify-start border-b-2 mx-10 my-5 py-5 "
-            key={`${idx}`}
-          >
-            <div className=" w-[40%] grid grid-cols-2  ">
-              <div className="  ">
-                <div className="font-bold ">ORDER No.</div>
-                <div className="">{e.order_number}</div>
-              </div>
-              <div className=" ">
-                <p className="font-bold">Payment</p>
-                <div className="">Credit Card</div>
-              </div>
-              <div className=" ">
-                <div className="font-bold">Delivery address</div>
-                <div className="">{address}</div>
-              </div>
-              <div className=" ">
-                <div className="font-bold">Email</div>
-                <div className="">{email}</div>
-              </div>
-            </div>
-            <div className=" text-sm px-5">
-              <div className="font-bold text-md">TOTAL</div>
-              <div className="text-xl font-bold ">€{getTotalCost(e.items)}</div>
-            </div>
-            <div className="w-[50%] ">{getItem(e.items)}</div>
-            {/* <div className="border-4 w-[50%]">{getOrderedItems()}</div> */}
-          </div>
-        );
+        return <OrderElement idx={idx} e={e} address={address} email={email} />;
       })}
-
-      {/* <OrderTemplate address={address} email={email} /> */}
     </div>
   );
 }
