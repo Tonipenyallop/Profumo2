8dd88832ced0f25e23f475b6e9ca6dff34bc45a2
Tonipenyallop <kimtesu521@gmail.com>
2022-08-26 15:11:29 +0900

    move file location

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+// import App from "./components/App";
 import App from "./components/App";
-import {Provider} from "react-redux"
-import {store} from "./app/store";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
-const container  = document.getElementById("root");
+const container = document.getElementById("root");
 const root = container && ReactDOM.createRoot(container);
 
-root && root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-    </Provider>
-  </React.StrictMode>
-);
+root &&
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>
+    </React.StrictMode>
+  );
