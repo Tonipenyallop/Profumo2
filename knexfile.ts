1d4ae8fcac2f6a5c6403973155193f0494f6e261
Tonipenyallop <kimtesu521@gmail.com>
2022-08-26 17:22:38 +0900

    update: add PORT

--- knexfile.ts
+++ knexfile.ts
@@ -32,6 +32,7 @@ const config: { [key: string]: Knex.Config } = {
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.PASSWORD,
+      port: 5432,
     },
     pool: {
       min: 2,
